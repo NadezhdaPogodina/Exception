62e82bd0ea6d406374b4d05b8fc3a65e7d48d5b9
NadezhdaPogodina <pogodinadia@inbox.ru>
2023-06-30 21:42:12 +0300

    Add files

---
+++ src/main/java/Smartphone.java
@@ -0,0 +1,19 @@
+import ru.netology.product.services.Product;
+
+
+public class Smartphone extends Product {
+
+    private String producer;
+
+    public Smartphone(int ID, String name, int price, String producer) {
+
+        super(ID, name, price);
+        this.producer = producer;
+    }
+
+    public String getProducer() {
+
+        return producer;
+    }
+
+}
